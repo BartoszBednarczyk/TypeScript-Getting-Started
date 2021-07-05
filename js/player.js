992530e748103f714436a1d5019e7678b04efabc
Bartosz <bednarczykbartosz@outlook.com>
2021-07-05 14:42:36 +0200

    Lesson 6

---
+++ js/player.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Player = void 0;
+var Player = (function () {
+    function Player() {
+    }
+    Player.prototype.formatName = function () {
+        return this.name.toUpperCase();
+    };
+    return Player;
+}());
+exports.Player = Player;
---
+++ js/utility.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.logger = exports.getValue = void 0;
+function getInputValue(elementID) {
+    var inputElement = document.getElementById(elementID);
+    return inputElement.value;
+}
+exports.getValue = getInputValue;
+function logger(message) {
+    console.log(message);
+}
+exports.logger = logger;
